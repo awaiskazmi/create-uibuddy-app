21bb5a9578283e65b7fefb94f7562ebe8ffa9c34
Awais Kazmi <40502100+awaiskazmi@users.noreply.github.com>
2021-08-30 16:13:18 +0500

    Update console messages

--- bin/app.js
+++ bin/app.js
@@ -41,10 +41,9 @@ async function main() {
     fs.rmdirSync(path.join(projectPath, "bin"), { recursive: true });
 
     console.log("Installaion complete!");
-    console.log("Run the following scripts in order:");
-    console.log("\n");
-    console.log("\x1b[32m%s\x1b[0m", "cd " + projectName);
-    console.log("\x1b[32m%s\x1b[0m", "npm run watch");
+    console.log("To get started:");
+    console.log("\x1b[32m%s\x1b[0m", "        cd " + projectName);
+    console.log("\x1b[32m%s\x1b[0m", "        npm run watch");
   } catch (error) {
     console.log(error);
   }
